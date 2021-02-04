0502608d43dce1de75145242e1dca5110c3c7520
jopinta <tatinfa@yahoo.com>
2021-02-04 23:45:28 +0000

    auto add

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char za;
+for (za = 'z' ; za >= 'a' ; za--)
+{
+putchar(za);
+}
+putchar('\n');
+return (0);
+}
