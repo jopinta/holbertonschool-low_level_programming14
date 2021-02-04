744d317ebd9a49da7cedbaca4407fbce6c65dfc9
jopinta <tatinfa@yahoo.com>
2021-02-04 23:26:17 +0000

    auto add

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char m;
+for (m = 0 ; m <= 9 ; i++)
+{
+putchar(m);
+}
+putchar('\n');
+return (0);
+}
