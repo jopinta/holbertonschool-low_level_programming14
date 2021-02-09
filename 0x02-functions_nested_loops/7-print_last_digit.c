5885c920e087b1e63d6ea960f14f42ebd473b399
jopinta <tatinfa@yahoo.com>
2021-02-09 22:16:06 +0000

    hoy

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _abs - compute the absolute value of an integer.
+ *@x: it's what we do
+ * Return: Always 0.
+ */
+
+int print_last_digit(int)
+
+	int _abs(int x)
+{
+	if (x < 0)
+	{
+		return (-x);
+	}
+
+	return (x);
+}
