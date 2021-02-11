a5bdaefcba2322b2fdd4002fce695b3f8e39117a
jopinta <tatinfa@yahoo.com>
2021-02-11 16:15:41 +0000

    multilied

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * mul - multiply to integers.
+ *@a: - mul
+ *@b: - multiplication.
+ * Return: (axb).
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
