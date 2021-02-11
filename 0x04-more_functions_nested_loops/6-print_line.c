40832f4e434af1fa95a2f1a023e81017dcfd1299
jopinta <tatinfa@yahoo.com>
2021-02-11 20:15:47 +0000

    love on hold

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_line - draw a straight line.
+ *@n : juee
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
