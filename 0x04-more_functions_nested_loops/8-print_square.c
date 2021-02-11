c8e4724ef6465196485369fec4fd76ff98db9f46
jopinta <tatinfa@yahoo.com>
2021-02-11 20:54:51 +0000

    fs

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+ #include "holberton.h"
+/**
+ * print_square - Entry point
+ * @size: number
+ * Description: print numbers from 0 to 14
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
