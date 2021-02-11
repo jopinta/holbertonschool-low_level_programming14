f8689ba83726c2f76569f81f3d9ab3409820cfa6
jopinta <tatinfa@yahoo.com>
2021-02-11 17:54:43 +0000

    jho

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,3 +6,17 @@
  * Return: Always 0.
  */
 void print_most_numbers(void)
+
+{
+	int a;
+
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		if (a == '2' || a == '4')
+		{
+			continue;
+		}
+		_putchar(a);
+	}
+	_putchar('\n');
+}
