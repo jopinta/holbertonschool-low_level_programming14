7475620a1fcd881c5eaf2048b9b41bf3c1f62246
jopinta <tatinfa@yahoo.com>
2021-02-19 19:19:30 +0000

    hu

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+ointers arrays strings
