ab06c42937d537f102779059ae80a3bc8349d5e0
jopinta <tatinfa@yahoo.com>
2021-02-19 21:52:09 +0000

    h

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * *string_toupper - Starwars
+ *
+ * @str: str
+ *
+ * Return: string
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] <= 'z' && str[i] >= 'a')
+			str[i] -= 32;
+	}
+	return (str);
+}
