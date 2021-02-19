0e97bb59e7eef1b122d1fbea3eff82163ed2547b
jopinta <tatinfa@yahoo.com>
2021-02-19 20:55:05 +0000

    f

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,9 +2,26 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _strncat - Str
+ * @dest: dest
+ * @src: src
+ * @n: num
+ * Return: dest
  */
-int main(void)
+
+char *_strncat(char *dest, char *src, int n)
 {
+	int z;
+	int x;
+
+	z = x = 0;
+	while (dest[z] != '\0')
+		z += 1;
+	while (x < n && src[x] != '\0')
+	{
+		dest[z] = src[x];
+		x += 1;
+		z += 1;
+	}
+	return (dest);
+}
