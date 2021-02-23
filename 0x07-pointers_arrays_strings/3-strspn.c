4527955e09a7b4eb2d008752bba3076ea2c7cf38
jopinta <tatinfa@yahoo.com>
2021-02-23 23:34:02 +0000

    we

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,17 +1,28 @@
-int ij, flag =o 
-char x
-j=0
-	for (i=0; s[1] !='\0'; i++)
-	for j =0 ; accept[j] != '\0'; j ++
+#include "holberton.h"
+/**
+ * _strspn - function that copies memory area
+ * @s: destiny character
+ * @accept: source character
+ *
+ * Return: number of bytes.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, ii = 0, bytes = 0;
+
+	for (; s[i] != '\0'; i++)
 	{
-		if (s[1]== accept[j]
+		for (; *(accept + ii) != '\0'; ii++)
+		{
+			if (s[i] == accept[ii])
 			{
-				x =& s[1];
-flag = 1
-break;
+				bytes++;
+				i++;
 			}
-			}
-		if (flag),
-break
-	return (x);
+			if (s[i] != accept[ii] && accept[ii] == '\0')
+				break;
+		}
+		ii = 0;
 	}
+	return (bytes);
+}
