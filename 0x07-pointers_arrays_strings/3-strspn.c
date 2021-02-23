e96e2cf75eadabb039ec452a785e02a5bb6be4a9
jopinta <tatinfa@yahoo.com>
2021-02-23 21:48:38 +0000

    fe

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,17 @@
+int ij, flag =o 
+char x
+j=0
+	for (i=0; s[1] !='\0'; i++)
+	for j =0 ; accept[j] != '\0'; j ++
+	{
+		if (s[1]== accept[j]
+			{
+				x =& s[1];
+flag = 1
+break;
+			}
+			}
+		if (flag),
+break
+	return (x);
+	}
