880ba921c746dfd0258f1a7bee09946b4ef7771b
jopinta <tatinfa@yahoo.com>
2021-02-23 23:19:13 +0000

    bob

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - check the code for Holberton School students.
+ *@to: ptr
+ *@s: double ptr
+ * Return: Always 0.
+ */
+void set_string(char **s, char *to)
+
+{
+*s = to;
+}
