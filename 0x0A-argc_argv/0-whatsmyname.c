f63405b2d2a669b88995a218213776b05aa5d69e
jopinta <tatinfa@yahoo.com>
2021-02-28 18:45:22 +0000

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the main fuction of the program
+ * @argc: the array index for each command flag
+ * @argv: the array of commands flags
+ * ----------------------------
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+argc = argc;
+
+printf("%s\n", argv[0]);
+
+return (0);
+}
