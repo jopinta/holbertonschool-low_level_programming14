02ef126162b81051cdcad565ad0ebf737879f6bb
jopinta <tatinfa@yahoo.com>
2021-02-28 20:16:36 +0000

    dy

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the main fuction of the program
+ * @argc: the array index for each command flag
+ * @argv: the array of commands flags
+ * ----------------------------
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+argv[0] = argv[0];
+
+printf("%d\n", argc - 1);
+
+return (0);
+}
