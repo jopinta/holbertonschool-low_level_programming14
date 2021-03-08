13a8da7822b296aa939a3a9242cd9aa4572af5d7
jopinta <tatinfa@yahoo.com>
2021-03-08 18:11:45 +0000

    g

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 /**
+ * main - program that prints the name of the file
  *
- *@void - similar
- *return: always
+ *@void: no parameters
+ *
+ * Return: Always 0 Succes
  */
+
 int main(void)
 {
 
 	printf("%s\n", __FILE__);
 	return (0);
-		}
+}
