0dcf85ace708a9300cec69fcdf68a58c569f3edf
jopinta <tatinfa@yahoo.com>
2021-03-23 22:47:48 +0000

    dd

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,7 +7,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *node;
 
-	if (!*head)
+	if (!head)
 		return;
 	while (*head)
 	{
@@ -15,4 +15,5 @@ void free_listint2(listint_t **head)
 		*head = (*head)->next;
 		free(node);
 	}
+	*head = NULL;
 }
