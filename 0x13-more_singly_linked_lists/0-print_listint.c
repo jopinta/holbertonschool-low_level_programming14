923de498c84195f2bf78c207720f50a6061003f8
jopinta <tatinfa@yahoo.com>
2021-03-23 22:18:39 +0000

    dreadzito

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,23 +1,22 @@
+  
 #include "lists.h"
 
 /**
- * print_listint - to print elementsof a list
- *@h: ptr to struct
- * Return: nodes
+ * print_listint - prints all elements of a list of ints
+ * @h: recieves a node
+ * -----------------------------------------
+ * Return: the number of nodes
  */
-
 size_t print_listint(const listint_t *h)
-
 {
-	size_t nodo = 0;
+	size_t count;
+	listint_t *current_h = (listint_t *) h;
 
-	while (h)
+	for (count = 0; current_h; count++)
 	{
-		printf("%i\n", h->n);
-		nodo++;
-
-		h = h->next;
+		printf("%d\n", current_h->n);
+		current_h = current_h->next;
 	}
 
-	return (nodo);
+	return (count);
 }
