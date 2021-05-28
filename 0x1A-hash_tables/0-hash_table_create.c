2ec5cd5742f8110e8292a81f85f1808b3edb786b
jopinta <tatinfa@yahoo.com>
2021-05-28 23:09:37 +0000

    hashin

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,11 +17,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new_t = malloc(sizeof(hash_table_t));
 /*devuelve puntero a espacio de memoria*/
+	if (!new_t)
+		return (NULL);
 
 	new_t->size = size;
 
 new_t->array = malloc(sizeof(hash_node_t **) * size);
-
+if (!new_t->array)
+{
+	free(new_t);
+		return (NULL);
+}
 	for (i = 0; i < size; i++)
 	{
 		new_t->array[i] = NULL;
